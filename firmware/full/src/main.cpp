e2562a40f3409e57beb2323d0c9993eea96445a7
Torino1564 <jtorinor6@gmail.com>
2024-08-02 19:52:41 -0300

    Agregado el sound manager. Soporta los numeros al reves

--- firmware/full/src/main.cpp
+++ firmware/full/src/main.cpp
@@ -50,9 +50,18 @@
 
 // Definiciones de tiempos
 #define DEBOUNCE_DELAY_MS 50
-#define LONG_PRESS_DELAY_MS 1000
+#define LONG_PRESS_DELAY_MS 600
 #define BOTH_PRESS_WINDOW_MS 500 // Ventana de tiempo para detectar ambos botones presionados
-#define UPDATE_RATE 100
+#define UPDATE_RATE 50
+
+// Definiciones de audio
+#define SHORT_PRESS_PWM 160
+#define LONG_PRESS_PWM 250
+#define DOUBLE_PRESS_PWM 255
+
+#define SHORT_PRESS_SFX_DURATION 150
+#define LONG_PRESS_SFX_DURATION 250
+#define DOUBLE_PRESS_SFX_DURATION 750
 
 // Enumeración de eventos de botones
 enum ButtonEvents
@@ -122,9 +131,55 @@ void setup()
     delay(10);
 }
 
+class SoundManager
+{
+public:
+    static void Tick()
+    {
+        auto& manager = Get_();
+
+        if (!manager.playing)
+            return;
+        
+        if ((millis() - manager.start_ms) > manager.duration_ms)
+        {
+            manager.playing = false;
+            manager.start_ms = 0;
+            manager.pwmIntensity = 0;
+            manager.duration_ms = 0;
+            analogWrite(PIN_BUZZER, 0);
+        }   
+    }
+    static void Play(const unsigned int pwmIntensity_, const unsigned long long duration_ms_)
+    {
+        auto& manager = Get_();
+
+        manager.playing = true;
+        manager.duration_ms = duration_ms_;
+        manager.pwmIntensity = pwmIntensity_;
+        manager.start_ms = millis();
+        analogWrite(PIN_BUZZER, pwmIntensity_); 
+    }
+private:
+    static SoundManager& Get_()
+    {
+        static SoundManager singleton;
+        return singleton;
+    }
+    SoundManager() = default;
+
+    // state
+    bool playing = false;
+    unsigned int pwmIntensity = 0;
+    unsigned long long duration_ms = 0;
+    unsigned long long start_ms = 0;
+};
+
 void loop()
 {
     ButtonEvents event = processButtons();
+
+    SoundManager::Tick();
 
     switch (event)
     {
@@ -132,31 +187,35 @@ void loop()
         DEBUG_PRINTLN("Pulsación corta en P1");
         count_p1++;
         count_p1 = (count_p1 > 99) ? 99 : count_p1;
-        analogWrite(PIN_BUZZER, 126);
+        SoundManager::Play(SHORT_PRESS_PWM, SHORT_PRESS_SFX_DURATION);
         break;
 
     case LONG_PRESS_P1:
         DEBUG_PRINTLN("Pulsación larga en P1");
         count_p1--;
         count_p1 = (count_p1 < 0) ? 0 : count_p1;
+        SoundManager::Play(LONG_PRESS_PWM, LONG_PRESS_SFX_DURATION);
         break;
 
     case SHORT_PRESS_P2:
         DEBUG_PRINTLN("Pulsación corta en P2");
         count_p2++;
         count_p2 = (count_p2 > 99) ? 99 : count_p2;
+        SoundManager::Play(SHORT_PRESS_PWM, SHORT_PRESS_SFX_DURATION);
         break;
 
     case LONG_PRESS_P2:
         DEBUG_PRINTLN("Pulsación larga en P2");
         count_p2--;
         count_p2 = (count_p2 < 0) ? 0 : count_p2;
+        SoundManager::Play(LONG_PRESS_PWM, LONG_PRESS_SFX_DURATION);
         break;
 
     case BOTH_LONG_PRESS:
         DEBUG_PRINTLN("Ambos botones presionados por mucho tiempo");
         count_p1 = 0;
         count_p2 = 0;
+        SoundManager::Play(DOUBLE_PRESS_PWM, DOUBLE_PRESS_SFX_DURATION);
         break;
 
     default:
@@ -286,15 +345,22 @@ bool showNumber(int number, bool isLeft)
     int digit1 = number / 10; // Dígito de las decenas
     int digit2 = number % 10; // Dígito de las unidades
 
+    // La logica esta pensada para que el orden de los numeros sea 
+    // [ 4 , 3 , 2 , 1 ]
+    // Para utilizar el codigo con orden de los numeros
+    // [ 1 , 2 , 3 , 4 ]
+    // Se debe cambiar el startIdx de isLeft en -28 y del else en +28. Ademas, se debe intercambiar el orden de digit1
+    // y digit2 en ambos caminos
+
     if (isLeft)
     {
-        showDigit(digit1, 0, CRGB::OrangeRed);  // Mostrar el primer dígito en el primer display del lado izquierdo
-        showDigit(digit2, 14, CRGB::OrangeRed); // Mostrar el segundo dígito en el segundo display del lado izquierdo
+        showDigit(digit2, 28, CRGB::OrangeRed);  // Mostrar el primer dígito en el primer display del lado izquierdo
+        showDigit(digit1, 42, CRGB::OrangeRed); // Mostrar el segundo dígito en el segundo display del lado izquierdo
     }
     else
     {
-        showDigit(digit1, 28, CRGB::BlueViolet); // Mostrar el primer dígito en el primer display del lado derecho
-        showDigit(digit2, 42, CRGB::BlueViolet); // Mostrar el segundo dígito en el segundo display del lado derecho
+        showDigit(digit2, 0, CRGB::BlueViolet); // Mostrar el primer dígito en el primer display del lado derecho
+        showDigit(digit1, 14, CRGB::BlueViolet); // Mostrar el segundo dígito en el segundo display del lado derecho
     }
 
     return true;
@@ -302,6 +368,16 @@ bool showNumber(int number, bool isLeft)
 
 bool showDigit(int digit, int startIdx, CRGB color)
 {
+    // El arreglo segmentMap mapea los numeros si el orden de los leds es el siguiente:
+    //  _ [ 1 ]_
+    // |6|    |2|
+    // |_|    |_|
+    //  _ [ 7 ]_
+    // |5|    |3|
+    // |_|    |_|
+    //    [ 4 ]
+
+
     static const uint8_t segmentMap[] = {
         B11111100, // 0
         B01100000, // 1
@@ -315,6 +391,28 @@ bool showDigit(int digit, int startIdx, CRGB color)
         B11100110  // 9
     };
 
+    // El arreglo segmentMap mapea los numeros si el orden de los leds es el siguiente:
+    //  _ [ 4 ]_
+    // |3|    |5|
+    // |_|    |_|
+    //  _ [ 7 ]_
+    // |2|    |6|
+    // |_|    |_|
+    //    [ 1 ]
+
+    static uint8_t segmentMapFlipped[] = {
+        B11111100, // 0
+        B00001100, // 1
+        B11011010, // 2
+        B10011110, // 3
+        B00101110, // 4
+        B10110110, // 5
+        B11110110, // 6
+        B00011100, // 7
+        B11111110, // 8
+        B00111110  // 9
+    };
+
     if (digit < 0 || digit > 9 || startIdx < 0 || startIdx >= NUM_LEDS)
     {
         return false;
@@ -323,7 +421,8 @@ bool showDigit(int digit, int startIdx, CRGB color)
     uint8_t mask = B10000000;
     for (int i = 0; i < 7; i++)
     {
-        if (segmentMap[digit] & mask)
+        // Reemplazar en esta linea de codigo por el arreglo que represente la orientacion correspondiente de los digitos
+        if (segmentMapFlipped[digit] & mask)
         {
             leds[startIdx + i * 2] = color;
             leds[startIdx + i * 2 + 1] = color;
